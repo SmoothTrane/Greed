d4793a682846dcce3598f4927cf7a48e1e033f90
Gary <gary.coltrane1@marist.edu>
2016-12-06 08:55:14 -0500

    added user input

--- game.rb
+++ game.rb
@@ -5,14 +5,35 @@ class Game
 	@grid = Grid.new
 	def new 
 		puts 'Hello, how are you? Let\'s play Grid!'
+		play
 	end
 
 
 
-def play
 
 
-end
+
+	def play
+		userInput = nil
+		until userInput === "q"
+			userInput = nil
+			until userInput === "n" || userInput === "e" || userInput === "s" || userInput === "w"
+				puts "Select if you want to go North South East or West"
+				userInput = gets.strip.downcase[0]
+			end
+
+			case userInput
+			when "n"
+				@grid.move("n")
+			when "e"
+				@grid.move("e")
+			when "s"
+				grid.move("s")
+			when "w"
+				grid.move("w")
+			end
+		end
+	end
 
 
 
